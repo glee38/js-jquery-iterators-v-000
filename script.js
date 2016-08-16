7adb97e700aef5358053af511eda605c51948d0e
glee38 <glee3388@gmail.com>
2016-08-15 21:32:13 -0400

    add html and jquery code

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 $(document).ready(function(){
 
-
+listIterate();
+  
 });
+
+var names = ["Carleton", "Avi", "Azat", "Ann" ]
+
+$.each(names, function(index, name){
+  console.log("hey " + name);
+});
+
+$('div').each(function(index, div){
+  $(this).append("this is div number " + (index + 1));
+});
+
+$.map(names, function(name, index){
+   return name + " loves JavaScript"; 
+});
+
+function listIterate(){
+  return $('li').map(function(item, index){
+      return item.innerHTML;
+  });
+}
